8f71abc68ba0c0a71fc592a097ccc777d4d00004
Cikida <misscgcali@gmail.com>
2022-06-25 15:28:28 +0200

    Changed alert to input prompt in helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,8 @@
-console.log("Git is Awesome!");
+let text;
+  let person = prompt("Please enter your name:", "Harry Potter");
+  if (person == null || person == "") {
+    text = "User cancelled the prompt.";
+  } else {
+    text = "Hello " + person + "! How are you today?";
+  }
+
